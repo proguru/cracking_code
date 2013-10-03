228e4047b9eb1add997f5e4a7ff4a8b90a567a86
proguru <prasanna@venneti-pc.(none)>
2013-10-02 18:01:21 -0700

    changed palindrome_in_string.c

--- Strings/palindrome_in_string.c
+++ Strings/palindrome_in_string.c
@@ -8,21 +8,29 @@
 int main(){
     char array[256];
     scanf("%s",array);
-   bool status=find_palindrome(array)
-    printf("status is %s",status ? "true":"false");
+    int status=find_palindrome(array);
+    char pr_string[10]="false";
+    if(status==0)
+        pr_string="true";
+    printf("status is %s",pr_string);
     return 0;
 }
 
-bool find_palindrome(char * array){
+int find_palindrome(char * array){
 
     int from_first;
     int from_last;
-    int mid=strlen(array)
-    printf("The size of the array is %d",array);
-    /*for(from_first=0,from_last=0;from_first<mid+1 && from_last>mid-1;from_first++,from_last--){
-
-    }*/
+    int mid=strlen(array);
+    printf("The size of the array is %d \n",mid);
+    int status=0;
+    for(from_first=0,from_last=0;from_first<mid+1 && from_last>mid-1;from_first++,from_last--){
+        if( array[from_first]!=array[from_last])
+        {
+            status=1;
+            break;           
+        }
+    }
     
-
+    return status; 
 
 }
