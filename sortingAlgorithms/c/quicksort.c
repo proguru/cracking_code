def5dbb32069aabc388428f3a89e2bbbed683681
proguru <venneti123@gmail.com>
2013-11-02 21:16:12 -0400

    modified quicksort.c

--- sortingAlgorithms/c/quicksort.c
+++ sortingAlgorithms/c/quicksort.c
@@ -7,7 +7,7 @@ Average case and best case if nlogn, but worst case is n^2.
 
 int main(){
     int a[10]={15,4,2,22,33,44,6,8,12,37};
-    array_size=sizeof(a)/sizeof(a[1]);
+    int array_size=sizeof(a)/sizeof(a[1]);
     quick_sort(a,array_size);
     int i;
     printf("\n");
@@ -23,9 +23,43 @@ void quick_sort(int *a,int array_size){
 }
 
 void _quick_sort(int *a,int left,int right,int pivot_index){
-   pivot_value=*(a+piviot_index);
-   // swap piviot vlaue with extreme right.
-   int temp=a[right];
-   a[right]=piviot_value;
-   a[pivot_index]=temp;  
+   if(right==left+1){
+       if(*(a+left)>*(a+right))
+            swap(a,left,right);
+   }
+   else{
+       int pivot_value=*(a+pivot_index);
+       // swap piviot vlaue with extreme right.
+       swap(a,pivot_index,right);
+       int i=0,big_index=-1;
+       for(i=left;i<right;i++){
+            if(a[i]>pivot_value){
+                if(big_index==-1)
+                    big_index=i;
+            }
+            else{
+                if(big_index!=-1){
+                    swap(a,big_index,i);
+                    big_index++;
+                }
+            }
+       }
+       if(big_index!=-1){
+            swap(*a,big_index,right);
+            int pivot_index_recur=(left+(big_index-1))/2;
+            _quick_sort(a,left,big_index-1,pivot_index_recur);
+            pivot_index_recur=((big_index+1)+right)/2;
+            _quick_sort(a,big_index+1,right,pivot_index_recur);
+        }
+        else{
+            int pivot_index_recur=(left+(right-1))/2;
+            _quick_sort(a,left,right-1,pivot_index_recur);
+        }
+    }   
+}
+
+void swap(int *a, int index1, int index2){
+    int temp=*(a+index1);
+    *(a+index1)=*(a+index2);
+    *(a+index2)=temp;
 }
